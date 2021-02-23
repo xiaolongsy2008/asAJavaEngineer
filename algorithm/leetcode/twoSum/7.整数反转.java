f085caf164c5ba7da19b3fda2d7df480aa7b9c7b
xuxiaolong002 <xiaolongsy2008@163.com>
2021-02-23 08:36:37 +0800

    整数反转--改进版

--- algorithm/leetcode/twoSum/7.整数反转.java
+++ algorithm/leetcode/twoSum/7.整数反转.java
@@ -6,30 +6,53 @@
 
 // @lc code=start
 class Solution {
-    public int reverse(int x) {
-        if(0==x){
+    // public int reverse(int x) {
+    //     if(0==x){
+    //         return 0;
+    //     }
+    //     if(0==x/10){
+    //         return x;
+    //     }
+    //     int result = 0;
+    //     Stack<Integer> sstack  = new Stack();
+    //     while(x/10!=0){
+    //       sstack.push(x%10);
+    //       x=x/10;
+    //     }
+    //     sstack.push(x);
+    //     int popTimes = 0;
+    //     while(!sstack.empty()){
+    //         result+=sstack.pop()*Math.pow(10,popTimes);
+    //         popTimes++;
+    //     }
+    //     if (result>=Integer.MAX_VALUE||result<=Integer.MIN_VALUE){
+    //         return 0;
+    //     }
+    //     return result;
+    // }
+
+
+public int reverse(int x){
+    int result = 0;
+    
+    while(x/10!=0){
+        int temp = x%10;
+        result = result*10 + temp;
+        if(result > Integer.MAX_VALUE/10){
+            return 0;
+        }else if(result < Integer.MIN_VALUE/10){
             return 0;
         }
-        if(0==x/10){
-            return x;
-        }
-        int result = 0;
-        Stack<Integer> sstack  = new Stack();
-        while(x/10!=0){
-          sstack.push(x%10);
-          x=x/10;
-        }
-        sstack.push(x);
-        int popTimes = 0;
-        while(!sstack.empty()){
-            result+=sstack.pop()*Math.pow(10,popTimes);
-            popTimes++;
-        }
-        if (result>=Integer.MAX_VALUE||result<=Integer.MIN_VALUE){
-            return 0;
-        }
-        return result;
+        x/=10;
     }
+    if(result == Integer.MAX_VALUE/10 && x>7){
+        return 0;
+    }else if(result==Integer.MIN_VALUE/10 && x< -8){
+        return 0;
+    }
+    result = result*10 + x;
+    return result;
+}
 }
 // @lc code=end
 
