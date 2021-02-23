dc0fc4e2333aece9cbf953f11c14702526dabe85
xuxiaolong002 <xiaolongsy2008@163.com>
2021-02-23 08:42:46 +0800

    while 终止条件

--- algorithm/leetcode/twoSum/7.整数反转.java
+++ algorithm/leetcode/twoSum/7.整数反转.java
@@ -35,22 +35,16 @@ class Solution {
 public int reverse(int x){
     int result = 0;
     
-    while(x/10!=0){
+    while(x!=0){
         int temp = x%10;
-        result = result*10 + temp;
-        if(result > Integer.MAX_VALUE/10){
+        if(result > Integer.MAX_VALUE/10 || result==Integer.MAX_VALUE/10 && temp > 7){
             return 0;
-        }else if(result < Integer.MIN_VALUE/10){
+        }else if(result < Integer.MIN_VALUE/10 || result==Integer.MIN_VALUE/10 && temp < -8){
             return 0;
         }
+        result = result*10 + temp;
         x/=10;
     }
-    if(result == Integer.MAX_VALUE/10 && x>7){
-        return 0;
-    }else if(result==Integer.MIN_VALUE/10 && x< -8){
-        return 0;
-    }
-    result = result*10 + x;
     return result;
 }
 }
